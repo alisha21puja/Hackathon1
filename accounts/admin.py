9531d64c4ddb1ca2c51dbbe3a98274f03a7e5fa3
ALWIN JOSEPH <47514997+Alwin1847207@users.noreply.github.comn>
2019-09-28 10:51:08 +0530

    chnages

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
+admin.site.register(LogEntry)
 
-# Register your models here.
+
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action_type', 'action_flag')
+    list_display_links = ('user', 'action_type')
+    search_fields = ('action_type',)
+    readonly_fields = ('content_type',
+                       'user',
+                       'action_time',
+                       'object_id',
+                       'object_repr',
+                       'action_flag',
+                       'change_message'
+                       )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_actions(self, request):
+        actions = super(LogEntryAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
